c6ce7183315d42237a1c427ca75047ebf2f5133b
fallen <rogerk08@126.com>
2018-10-14 02:19:35 +0800

    *

--- src/main/java/com/fallen/parrot/mybatis/impl/slave1/user/SlaveUserThirdAccessImpl.java
+++ src/main/java/com/fallen/parrot/mybatis/impl/slave1/user/SlaveUserThirdAccessImpl.java
@@ -1,5 +1,7 @@
 package com.fallen.parrot.mybatis.impl.slave1.user;
 
+import com.fallen.parrot.annotation.DataSource;
+import com.fallen.parrot.config.DataSourceContextHolder;
 import com.fallen.parrot.mybatis.entity.UserThirdAccessEntity;
 import com.fallen.parrot.mybatis.impl.slave1.BaseServiceImpl;
 import com.fallen.parrot.mybatis.mapper.UserThirdAccessMapper;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SlaveUserThirdAccessImpl extends BaseServiceImpl<UserThirdAccessEntity, UserThirdAccessMapper> {
 
+    @DataSource(DataSourceContextHolder.Slaver1)
     public UserThirdAccessEntity findByUserId(Long userId, int type) {
         return getMapper().findByUserId(userId, type);
     }
